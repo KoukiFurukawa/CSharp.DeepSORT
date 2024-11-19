4967521c68cb7baebf0dfde6768340b97fcc5e04
kouki928 <furukawavo@gmail.com>
2024-11-19 17:38:25 +0900

    first commit

---
+++ CSharp.DeepSORT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Csharp.DeepSORT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
