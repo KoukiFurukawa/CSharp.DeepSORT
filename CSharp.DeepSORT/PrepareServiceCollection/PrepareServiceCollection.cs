dfe1043ddf0e47ff60b95ceceff18b7a6aced6b3
kouki928 <furukawavo@gmail.com>
2024-12-29 00:12:16 +0900

    DI処理を追加

---
+++ CSharp.DeepSORT/PrepareServiceCollection/PrepareServiceCollection.cs
@@ -0,0 +1,47 @@
+﻿using CSharp.DeepSORT.ViewModels;
+using DeepSORT.Application.DetectorUseCase;
+using DeepSORT.Domain.Models.Detector;
+using DeepSORT.Domain.Models.WebCamera;
+using DeepSORT.Infrastructure.Factory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CSharp.DeepSORT;
+static public class PrepareServiceCollection
+{
+    private static void RegisterViewModel(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<MainWindowViewModel>();
+    }
+
+    private static void RegisterInfrastructure(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IDetectorFactory, DetectorFactory>();
+        serviceCollection.AddSingleton<IWebCameraFactory, WebCameraFactory>();
+    }
+
+    private static void RegisterService(IServiceCollection serviceCollection)
+    {
+    }
+
+    private static void RegisterUseCase(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<DetectorUseCase>();
+    }
+
+    public static IServiceProvider Initialize()
+    {
+        // 変数宣言
+        IServiceCollection serviceCollection = new ServiceCollection();
+        IServiceProvider serviceProvider;
+
+        // 登録
+        RegisterInfrastructure(serviceCollection);
+        RegisterService(serviceCollection);
+        RegisterUseCase(serviceCollection);
+        RegisterViewModel(serviceCollection);
+
+        serviceProvider = serviceCollection.BuildServiceProvider();
+
+        return serviceProvider;
+    }
+}
