9894cc9f2cf288de5de9c31a09b80e019424f50e
kouki928 <furukawavo@gmail.com>
2025-01-17 09:40:12 +0900

    MainWIndow の出現方法を変更

--- CSharp.DeepSORT/MainWindow.xaml.cs
+++ CSharp.DeepSORT/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using DeepSORT.Domain.Models.Detector;
-using DeepSORT.Domain.Models.Predictor;
-using OpenCvSharp;
-using System.Diagnostics;
-using System.Text;
+﻿using CSharp.DeepSORT;
+using CSharp.DeepSORT.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Csharp.DeepSORT
 {
@@ -20,11 +10,14 @@ namespace Csharp.DeepSORT
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
+        public static IServiceProvider? ServiceProvider { get; private set; }
         /*        private readonly ModelPath _mockModelPath;
                 private readonly string IMAGE_PATH = AppContext.BaseDirectory + "./images/zidane.jpg";
                 private readonly string IMAGE_PATH2 = AppContext.BaseDirectory + "./images/dog.jpeg";*/
         public MainWindow()
         {
+            ServiceProvider = PrepareServiceCollection.Initialize();
+            this.DataContext = ServiceProvider.GetRequiredService<MainWindowViewModel>();
             InitializeComponent();
             /*            string modelPath = AppContext.BaseDirectory + "./models/yolo11n.onnx";
                         *//*string modelPath2 = AppContext.BaseDirectory + "./models/resnet18-v1-7.onnx";*//*
@@ -48,6 +41,16 @@ namespace Csharp.DeepSORT
                         {
                             Debug.WriteLine(ex.Message);
                         }*/
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+            if (MessageBoxResult.Yes != MessageBox.Show("画面を閉じます。よろしいですか？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Information))
+            {
+                e.Cancel = true;
+                return;
+            }
         }
     }
 }
