4b1cee7d9c8ab5351b04f5e09276804718ca7c50
kouki928 <furukawavo@gmail.com>
2025-01-28 09:57:16 +0900

    画面サイズを拡大

---
+++ CSharp.DeepSORT/Models/RequestButton.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace CSharp.DeepSORT.Models;
+public class RequestButton : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+/*    public BitmapSource? Image
+    {
+        get => _image;
+        set
+        {
+            _image = value;
+            OnPropertyChanged(nameof(Image));
+        }
+    }*/
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
